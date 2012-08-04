2623026f39ada722dcdfec4ff54b4ddb9691aeb2
Jerome Etienne <jerome.etienne@gmail.com>
2012-08-04 10:32:35 +0200

    added pageTitle + updated tQuery

--- vendor/tquery/tquery-bundle.js
+++ vendor/tquery/tquery-bundle.js
@@ -1849,9 +1849,16 @@ fragmentShader:"precision mediump float;\nuniform vec3 color;\nuniform sampler2D
 */
 var tQuery	= function(object, root)
 {
-	// support for tQuery(tGeometry, tMaterial)
-	if( arguments.length === 2 && arguments[0] instanceof THREE.Geometry && arguments[1] instanceof THREE.Material ){
-		return tQuery(new THREE.Mesh(arguments[0], arguments[1]))
+	// support for tQuery(geometry, material)
+	if( arguments.length === 2 && 
+			(arguments[0] instanceof THREE.Geometry || arguments[0] instanceof tQuery.Geometry)
+			&& 
+			(arguments[1] instanceof THREE.Material || arguments[1] instanceof tQuery.Material)
+			){
+		var tGeometry	= arguments[0] instanceof tQuery.Geometry ? arguments[0].get(0) : arguments[0];
+		var tMaterial	= arguments[1] instanceof tQuery.Material ? arguments[1].get(0) : arguments[1];
+		var tMesh	= new THREE.Mesh(tGeometry, tMaterial);
+		return tQuery( tMesh );
 	}
 
 // TODO make tthat cleaner
@@ -2392,6 +2399,21 @@ tQuery.mixinAttributes(tQuery.Object3D, {
 	receiveShadow		: tQuery.convert.toBool,
 	castShadow		: tQuery.convert.toBool
 });
+
+/**
+ * Traverse the hierarchy of Object3D. 
+ * 
+ * @returns {tQuery.Object3D} return the tQuery.Object3D itself
+*/
+tQuery.Object3D.prototype.traverseHierarchy	= function(callback){
+	this.each(function(object3d){
+		THREE.SceneUtils.traverseHierarchy(object3d, function(object3d){
+			callback(object3d);
+		});
+	});
+	return this;	// for chained API
+};
+
 
 //////////////////////////////////////////////////////////////////////////////////
 //		geometry and material						//
@@ -2943,7 +2965,7 @@ tQuery.World.prototype.destroy	= function(){
 	// microevent.js notification
 	this.trigger('destroy');
 	// unhook the render function in this._loop
-	this._loop.hookOnRender(this._$loopCb);
+	this._loop.unhookOnRender(this._$loopCb);
 	// destroy the loop
 	this._loop.destroy();
 	// remove this._cameraControls if needed
@@ -3986,13 +4008,34 @@ tQuery.World.register('boilerplate', function(opts){
 	return this;
 });
 
+/**
+ * Define a page title
+*/
+tQuery.World.register('pageTitle', function(element){
+	// handle parameters polymorphism
+	if( typeof(element) === 'string' ){
+		var element	= document.querySelector(element);
+	}
+	// sanity check
+	console.assert( element instanceof HTMLElement);
+	// set element.style
+	element.style.position	= "absolute";
+	element.style.width	= "100%";
+	element.style.textAlign	= "center";
+	element.style.fontWeight= "bolder";
+	element.style.fontColor	= "white";
+	element.style.paddingTop= "0.5em";
+	element.style.fontFamily= "arial";
+	// for chained API
+	return this;
+});
+
 tQuery.World.register('addBoilerplate', function(opts){
 	var _this	= this;
 	// sanity check - no boilerplate is already installed
 	console.assert( this.hasBoilerplate() !== true );
 	// handle parameters	
 	opts	= tQuery.extend(opts, {
-		honorInfo	: true,
 		stats		: true,
 		cameraControls	: true,
 		windowResize	: true,
@@ -4008,17 +4051,6 @@ tQuery.World.register('addBoilerplate', function(opts){
 
 	// create the context
 	tQuery.data(this, '_boilerplateCtx', ctx);
-
-	// add css for the info element if any
-	if( opts.honorInfo ){
-		var element	= document.getElementById('info');
-		if( element ){
-			element.style.position	= "absolute";
-			element.style.width	= "100%";
-			element.style.textAlign	= "center";
-		}
-	}
-
 
 	// get some variables
 	var tCamera	= this.tCamera();
